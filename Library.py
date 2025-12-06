bd280c7d52a5c02880f76088259ceec0c6b875d8
Ngọc Thanh <truongthingocthanh111106@gmail.com>
2025-12-06 14:36:28 +0700

    Feature: Implement add_book function

--- Library.py
+++ Library.py
@@ -1,7 +1,26 @@
 library = []
-
 def main():
     pass  
+if __name__ == "__main__":
+    main()
+library = []
 
+def add_book():
+    print("--- Enter book information ---")
+    book_id = input("Enter the book code: ")
+    title = input("Enter the book title: ")
+    author = input("Enter the author's name: ")
+    
+    book = {
+        "id": book_id,
+        "title": title,
+        "author": author
+    }
+    
+    library.append(book)
+    print(f"Books added '{title}' entered the library successfully!")
+
+def main():
+    add_book() 
 if __name__ == "__main__":
     main()
